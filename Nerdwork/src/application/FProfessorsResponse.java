a13b3ffd5e5af7a4a6ed910c6fcb2a3c14cd0066
geoxhon <geoxhon@gmail.com>
2022-06-03 21:39:04 +0300

    Update FProfessorsResponse.java

--- Nerdwork/src/application/FProfessorsResponse.java
+++ Nerdwork/src/application/FProfessorsResponse.java
@@ -6,13 +6,15 @@ public class FProfessorsResponse {
     public String phone;
     public String email;
     public String profilePhoto;
+    public String office;
     public float rating;
-    public FProfessorsResponse(int id, String name, String phone, String email, String profilePhoto, float rating) {
+    public FProfessorsResponse(int id, String name, String phone, String email, String profilePhoto, String office, float rating) {
     	this.id = id;
     	this.name = name;
     	this.phone = phone;
     	this.rating = rating;
     	this.email = email;
+    	this.office = office;
     	this.profilePhoto = profilePhoto;
     }
 }
