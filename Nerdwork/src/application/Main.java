b042a516d8aff0c6be7fc612feb379e28e1f660c
ptsionis <tsionispanagiotis@gmail.com>
2022-06-05 17:03:18 +0300

    Added tests for GuiController methods in Main

--- Nerdwork/src/application/Main.java
+++ Nerdwork/src/application/Main.java
@@ -8,15 +8,73 @@ import org.json.simple.parser.ParseException;
 public class Main  {
 	
 	public static void main(String[] args) throws IOException, ParseException {
-		URestController controller = new URestController();
-		ArrayList<FSubjectsResponse> fsr;
-		ArrayList<Course> courses = new ArrayList<Course>();
+		//GUI Controller creation
+		GuiController controller = new GuiController();
 		
-		//System.out.println(controller.doRegister("iss1234", "123456789", "iss1234", "iss1234@uom.edu.gr"));
+		//Login section
+		System.out.print("Login: ");
+		System.out.println(controller.login("iss1234", "123456789"));
+		System.out.println("--------------------");
 		
-		System.out.println("Login: ");
-		System.out.println(controller.doLogin("iss1234", "123456789"));
+		//All courses section
+		System.out.println("All courses in the database: ");
+		for (Course c : controller.getAllCourses()) {
+			System.out.println();
+			System.out.println("Id: " + c.getId() + ", " + "Name: " + c.getName() + ", " + "Semester: " + c.getSemester()
+			+ ", " + "ECTS: " + c.getECTS() + ", " + "Rating: " + c.getRating());
+		}
+		System.out.println("--------------------");
 		
-		System.out.println("\nAppointment Tests: \n");
+		//Enrolled courses section
+		System.out.println("My enrolled courses: ");
+		for (Course c : controller.getEnrolledCourses()) {
+			System.out.println();
+			System.out.println("Id: " + c.getId() + ", " + "Name: " + c.getName() + ", " + "Semester: " + c.getSemester()
+			+ ", " + "ECTS: " + c.getECTS() + ", " + "Rating: " + c.getRating() + ", " + "My rating: " + controller.getMyCourseRating(c.getId()));
+		}
+		System.out.println("--------------------");
+		
+		//Enroll to a course
+		System.out.println("Enroll to a course (AIC106): ");
+		System.out.println("Enrolling to AIC106: " + controller.courseEnrollment("AIC106"));
+		System.out.println("My NEW enrolled courses (AIC106 included): ");
+		for (Course c : controller.getEnrolledCourses()) {
+			System.out.println();
+			System.out.println("Id: " + c.getId() + ", " + "Name: " + c.getName() + ", " + "Semester: " + c.getSemester()
+			+ ", " + "ECTS: " + c.getECTS() + ", " + "Rating: " + c.getRating() + ", " + "My rating: " + controller.getMyCourseRating(c.getId()));
+		}
+		System.out.println("--------------------");
+		
+		//Disenroll from a course
+		System.out.println("Disenroll to a course (AIC106): ");
+		System.out.println("Disenrolling to AIC106: " + controller.courseDisenrollment("AIC106"));
+		System.out.println("My NEW enrolled courses (AIC106 NOT included): ");
+		for (Course c : controller.getEnrolledCourses()) {
+			System.out.println();
+			System.out.println("Id: " + c.getId() + ", " + "Name: " + c.getName() + ", " + "Semester: " + c.getSemester()
+			+ ", " + "ECTS: " + c.getECTS() + ", " + "Rating: " + c.getRating() + ", " + "My rating: " + controller.getMyCourseRating(c.getId()));
+		}
+		System.out.println("--------------------");
+		
+		//All professors section
+		System.out.println("All professors in the database: ");
+		for (Professor p : controller.getAllProfessors()) {
+			System.out.println();
+			System.out.println("ProfId: " + p.getProfessorId() + ", " + "UserId: " + p.getUserId() + ", " +"Name: " + p.getDisplayName() + ", "
+			+ "Email: " + p.getEmail() + ", " + "Office: " + p.getOffice() + ", " + "Phone: " + p.getPhone() + ", " 
+			+ "Photo: " + p.getProfilePhoto() + ", " + "Rating: " + p.getRating() + ", " + "My rating: " + controller.getMyProfessorRating(p.getProfessorId()));
+		}
+		System.out.println("--------------------");
+		
+		//Rate a professor
+		System.out.println("Rating professor 14 (achat), 4 stars: ");
+		System.out.println("This should return false in case I have already rated professor 14: " + controller.rateProfessor(4, 14));
+		System.out.println("Professor 14 rating, my rating:");
+		System.out.println(controller.getAllProfessors().get(13).getDisplayName() + ", " + "Rating: "
+		+ controller.getAllProfessors().get(13).getRating() + ", " + "My rating: " + controller.getMyProfessorRating(14));
+		System.out.println("--------------------");
+		
+		//To be continued...
 	}
+
 }
