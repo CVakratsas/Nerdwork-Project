fe437bf4d9bce5e53f5a2ae39a18560ecc9adf79
PanosMavv <102112065+PanosMavv@users.noreply.github.com>
2022-06-18 01:27:52 +0300

    Updated Appointments GUI to list all Professor's available appointments

--- Nerdwork/src/application/gui/classes/AppointmentsController.java
+++ Nerdwork/src/application/gui/classes/AppointmentsController.java
@@ -1,7 +1,11 @@
 package application.gui.classes;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.json.simple.parser.ParseException;
 
@@ -9,6 +13,7 @@ import application.functionality.GuiController;
 import application.functionality.Professor;
 import application.functionality.Timeslot;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -32,6 +37,8 @@ public class AppointmentsController {
 	private Label currentProfessorOffice;
 	@FXML
 	private VBox professorList;
+	@FXML
+	private HBox appointmentList;
 	
 	
 	public void initialize() throws IOException, ParseException {
@@ -63,27 +70,110 @@ public class AppointmentsController {
 			//Loads selected Professor's available appointments
 			box.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> {
 				try {
-					Professor chosenProfessor = controller.getProfessorById(Integer.parseInt(box.getId()));
-					currentProfessorPicture.setImage(new Image(dbURL + chosenProfessor.getProfilePhoto()));
-					currentProfessorName.setText(chosenProfessor.getDisplayName());
-					currentProfessorOffice.setText(chosenProfessor.getOffice());
+					currentProfessor = controller.getProfessorById(Integer.parseInt(box.getId()));
+					currentProfessorPicture.setImage(new Image(dbURL + currentProfessor.getProfilePhoto()));
+					currentProfessorName.setText(currentProfessor.getDisplayName());
+					currentProfessorOffice.setText(currentProfessor.getOffice());
 					
-					loadAppointments(chosenProfessor);
+					loadAppointments();
 					
-				} catch (NumberFormatException | IOException | ParseException e) {e.printStackTrace();}
+				} catch (NumberFormatException | IOException | ParseException | java.text.ParseException e) {e.printStackTrace();}
 			});
 			
 			professorList.getChildren().add(box);
 		}
-	}
+	}	
 	
+
+	private void loadAppointments() throws IOException, ParseException, java.text.ParseException {
+		appointmentList.getChildren().clear();
+		
+		ArrayList<Timeslot> timeslots = controller.getAvailableTimeslots(currentProfessor);
+		appointmentList.setSpacing(15);
+		
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		
+		if(timeslots != null) {
+			Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+			
+			
+			
+			while(!timeslots.isEmpty())
+				for(String day : Timeslot.Days) {
 	
+					//Timeslots of the day
+					ArrayList<Timeslot> dayTimeslots = new ArrayList<>();
 	
-	private void loadAppointments(Professor p) throws IOException, ParseException {
-		for(Timeslot t : controller.getAvailableTimeslots(p)) {
-			System.out.println(new Date(((long) t.getStartHourTimestamp()) * 1000));
-			System.out.println(new Date(((long) t.getEndHourTimestamp()) * 1000));
-			System.out.println("\n");
-		}
+					
+					//Selects all timeslots of a certain day of the week
+					for(Timeslot t : timeslots) {
+						Date startHourDate = new Date(((long) t.getStartHourTimestamp()) * 1000);
+						calendar.setTime(startHourDate);
+						
+						
+						String weekDay = Timeslot.Days[calendar.get(Calendar.DAY_OF_WEEK) - 1];
+						if(!weekDay.equals(day))
+							break;
+						
+						dayTimeslots.add(t);
+					}
+					
+					
+					//Removes selected timeslots from all timeslots
+					for(Timeslot t : dayTimeslots) {
+						timeslots.remove(t);
+					}
+					
+					
+					
+					if(dayTimeslots.isEmpty()) //No appointment was found for a certain day
+						continue;
+					else {
+						Date startHourDate = new Date(((long) dayTimeslots.get(0).getStartHourTimestamp()) * 1000);
+						calendar.setTime(startHourDate);
+					}
+						
+					
+					//Creates a new Container to render all available appointments for all available dates
+					VBox appointment = new VBox();
+					appointment.setSpacing(15);
+					
+					
+					String dateStr = formatter.format(calendar.getTime());
+					String weekDayStr = Timeslot.Days[calendar.get(Calendar.DAY_OF_WEEK) - 1];
+					
+					
+					Label date = new Label(dateStr);
+					Label weekDay = new Label(weekDayStr);
+					
+					appointment.getChildren().add(date);
+					appointment.getChildren().add(weekDay);
+					
+					
+					for(Timeslot t : dayTimeslots) {
+						
+						Date startDate = new Date(((long) t.getStartHourTimestamp()) * 1000);
+						calendar.setTime(startDate);
+						String startTime = String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
+						
+						
+						Date endDate = new Date(((long) t.getEndHourTimestamp()) * 1000);
+						calendar.setTime(endDate);
+						String endTime = String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
+						
+						
+						String timeRange = startTime + " - " + endTime;
+						
+						
+						Button appointmentButton = new Button(timeRange);
+						appointment.getChildren().add(appointmentButton);
+					}
+					
+					appointmentList.getChildren().add(appointment);
+				}
+		}	
 	}
 }
