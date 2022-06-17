ade078defc8b2fb409f7f99dae888141776131a9
geoxhon <geoxhon@gmail.com>
2022-06-17 16:33:06 +0300

    Added support for orientation

--- Nerdwork/src/application/FLoginResponse.java
+++ Nerdwork/src/application/FLoginResponse.java
@@ -1,3 +1,4 @@
+package application.api;
 
 public class FLoginResponse{
 	public boolean isSuccess;
@@ -6,15 +7,17 @@ public class FLoginResponse{
 	public String username;
 	public int accountType;//Για φοιτητες accountType=0, για καθηγητες accountType = 1
 	public int associatedProfessorId; 
+	public int orientation; //Για ΕΤΥ = 0, Για ΠΣ = 1
 	public FLoginResponse(boolean success) {
 		isSuccess = success;
 	}
-	public FLoginResponse(boolean success, String id, String Name, String username, int accountType, int associatedProfessorId) {
+	public FLoginResponse(boolean success, String id, String Name, String username, int accountType, int associatedProfessorId, int orientation) {
 		isSuccess = success;
 		userId = id;
 		displayName = Name;
 		this.username = username;
 		this.accountType = accountType;
 		this.associatedProfessorId = associatedProfessorId;
+		this.orientation = orientation;
 	}
 }
