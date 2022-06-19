49d396f5a4d17cfc9a5f60dd444c2165994b2ae3
geoxhon <geoxhon@gmail.com>
2022-06-19 13:23:56 +0300

    Added PHP API

---
+++ Extras/PHP/config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'u936088023_nerdnet');
+define('DB_PASSWORD', 'Kr9ZZ6CpPXXUd99');
+define('DB_NAME', 'u936088023_nerdnet');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
